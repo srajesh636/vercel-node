8d36c2c62181fb7e870670d9c1797677791074ba
U Sai Rajesh <srajesh636@gmail.com>
2024-05-14 15:15:59 +0530

    return validation token in

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/ping", (req, res) => res.send("Express on Vercel"));
+app.get("/ping", (req, res) => {
+    const validationToken = req.query.validationToken;
+    res.status(200).send(validationToken);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
