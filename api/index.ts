85b6cf3aae2f5a718686f45c9928557b4d96a93e
U Sai Rajesh <srajesh636@gmail.com>
2024-05-15 16:20:23 +0530

    modify

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/ping", (req, res) => {
+app.post('/webhook', (req, res) => {
+    console.log("data",req.body,req.query)
     const validationToken = req.query.validationToken;
     res.set('Content-Type', 'text/plain');
     res.status(200).send(validationToken);
 });
-
-app.post('/webhook', (req, res) => {
-    console.log("data",req.body)
-    res.status(200)
-});
-
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
