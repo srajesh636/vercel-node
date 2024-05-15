119a95db2c08fc8e5b875b89a45c4fe012d15115
U Sai Rajesh <srajesh636@gmail.com>
2024-05-15 14:49:37 +0530

    log

--- api/index.ts
+++ api/index.ts
@@ -9,17 +9,14 @@ app.get("/ping", (req, res) => {
 
 app.post('/webhook', (req, res) => {
     const validationToken = req.query.validationToken;
-    console.log("Request",req)
-    res.set('Content-Type', 'text/plain');
-    res.status(200).send(validationToken);
+    if(validationToken){
+        res.set('Content-Type', 'text/plain');
+        return res.status(200).send(validationToken);
+    }
+    console.log(req.body)
+    res.status(200)
 });
 
-app.get('/webhook', (req, res) => {
-    const validationToken = req.query.validationToken;
-    console.log("Request",req)
-    res.set('Content-Type', 'text/plain');
-    res.status(200).send(validationToken);
-});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
